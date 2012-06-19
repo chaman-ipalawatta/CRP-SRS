db0c5280b506a0245ffc3bb10696096c5c728c80
Chaman Ipalawatta <chaman@gmail.com>
2012-06-19 14:15:19 +1000

    Revert "Initial Commit"
    This reverts commit c12d3daab49e0133a949eeb0cca7587206011221.

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "include/menu.php.inc";
-include "include/footer.php.inc";
-?>
-<div id="report-pos1"><?php include "include/monthlyRepairCount.php"; ?></div>
-<div id="report-pos2"><?php include "include/mostServicedVehicles.php"; ?></div>
-<div id="report-pos3-head"><h3>Due Services - Next 7 Days</h3></div>
-<div id="report-pos3"><?php include "include/serviceDues.php"; ?></div>
